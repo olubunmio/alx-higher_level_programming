09b28fdb6fb5d802b1646bf3da66080bd2fdbc0e
Olubunmi Onwuemene <olubunmi.onwuemene@gmail.com>
2022-11-29 10:38:46 -0800

    1

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,9 +6,9 @@ if number < 0:
 else:
     last_digit = int(repr(number)[-1])
 if last_digit > 5:
-    print(f"Last digit of {number} is {last_digit} and is greater than 5")
+    print(f"Last digit of {number:d} is {last_digit:d} and is greater than 5")
 elif last_digit == 0:
     print(f"Last digit of {number:d} is {last_digit:d} and is 0")
 elif last_digit < 6 and last_digit != 0:
-    print(f"Last digit of {number:d} is\
-          {last_digit:d} and is less than 6 and not 0")
+    print(f"Last digit of {number:d} is {last_digit:d} and is less "
+          "than 6 and not 0")
